ec7212e445d5d1b655c014ffa614fe7635122106
Jeff Glass <glass.jeffrey@gmail.com>
2023-09-26 13:28:20 -0500

    Add config spec

--- mkdocs-pyscript/plugin.py
+++ mkdocs-pyscript/plugin.py
@@ -4,14 +4,13 @@ from timeit import default_timer as timer
 from datetime import datetime, timedelta
 
 from mkdocs import utils as mkdocs_utils
-from mkdocs.config import config_options, Config
+from mkdocs.config import config_options, Config, config_options
 from mkdocs.plugins import BasePlugin
 
-class YourPlugin(BasePlugin):
+class MyPluginConfig(mkdocs.config.base.Config):
+    version = config_options.Type(str, default='2023.09.1')
 
-    config_scheme = (
-        ('param', config_options.Type(str, default='')),
-    )
+class YourPlugin(BasePlugin[MyPluginConfig]):
 
     def __init__(self):
         self.enabled = True
