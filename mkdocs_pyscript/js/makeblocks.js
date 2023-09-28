bb05207fd858fa01b069a704fb6260af78ad143d
Jeff Glass <glass.jeffrey@gmail.com>
2023-09-28 09:49:49 -0500

    Run code in worker

--- mkdocs_pyscript/js/makeblocks.js
+++ mkdocs_pyscript/js/makeblocks.js
@@ -5,6 +5,7 @@ import { indentUnit } from 'https://cdn.jsdelivr.net/npm/@codemirror/language@6.
 import { keymap } from 'https://cdn.jsdelivr.net/npm/@codemirror/view@6.9.3/+esm';
 import { defaultKeymap } from 'https://cdn.jsdelivr.net/npm/@codemirror/commands@6.2.2/+esm';
 import {$, $$} from 'https://cdn.jsdelivr.net/npm/basic-devtools@0.1.6/+esm'
+import { PyWorker } from "https://cdn.jsdelivr.net/npm/@pyscript/core";
 
 const RUNBUTTON = `<svg style="height:20px;width:20px;vertical-align:-.125em;transform-origin:center;overflow:visible;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>`;
 
@@ -146,9 +147,34 @@ class PyRepl extends HTMLElement {
      *  display() the last evaluated expression
      */
     async execute() {
-        const pySrc = this.getPySrc();
-        
+        const pySrc = `
+import sys
+from pyscript import sync
+
+class MyStdout:
+    def write(self, line):
+        sync.write(line)
+
+class MyStderr:
+    def write(self, line):
+        sync.writeErr(line)
+
+sys.stdout = MyStdout()  
+sys.stderr = MyStderr()   
+
+${this.getPySrc()}
+`;
+
         console.warn(`Running code ${pySrc}`)
+
+        const srcLink = URL.createObjectURL((new Blob([pySrc])))
+        console.log(srcLink)
+
+        this.outDiv.innerHTML = ""
+        const worker = PyWorker(srcLink, {hooks: {onWorkerReady: 'console.log.("worker is GO")'}});
+        worker.sync.write = (str) => {this.outDiv.innerText += str}
+        worker.sync.writeErr = (str) => {this.outDiv.innerHTML += `<span style='color:red'>${str}</span>`}
+        worker.onerror = ({error}) => {this.outDiv.innerHTML += `<span style='color:red'>${str}</span>`; console.log(error)}
     }
 
     getPySrc() {
@@ -196,12 +222,16 @@ function replaceWithEditor(pySrc, parent){
     parent.innerHTML = ""
     parent.appendChild(repl)
 
+    console.log({pySrc})
+
     //Insert PySrc
-    repl.editor.state.update({changes: {
+    repl.editor.dispatch({changes: {
         from: 0,
         to: repl.editor.state.doc.length,
         insert: pySrc
     }})
+    
+    console.log("Done setting source/")
 }
 
 addButtons();
