f0e9798e20c0e518a4985dbfd767fb22d3ad9b99
Jeff Glass <glass.jeffrey@gmail.com>
2023-09-27 22:06:06 -0500

    Working mini-coi injection

--- mkdocs_pyscript/plugin.py
+++ mkdocs_pyscript/plugin.py
@@ -3,13 +3,17 @@ from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options, Config, config_options, base
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import BasePlugin, get_plugin_logger
-from mkdocs.structure.files import Files
+from mkdocs.structure.files import Files, File
 from mkdocs.structure.pages import Page
 from mkdocs.utils.templates import TemplateContext
 
 from typing import Any
 
 from bs4 import BeautifulSoup
+import tempfile
+from pathlib import Path
+import os
+from .minicoi import mini_coi_content
 
 DEFAULT_VERSION = "2023.09.1.RC1"
 SCRIPT = 'https://pyscript.net/snapshots/{version}/core.js'
@@ -22,12 +26,16 @@ class Plugin(BasePlugin[MyPluginConfig]):
     def __init__(self):
         self.enabled = True
         self.total_time = 0
+        self.temp_path = "./_mkdocs_pyscript_tmp/mini_coi.js"
+        if not os.path.exists("./_mkdocs_pyscript_tmp"): os.mkdir("./_mkdocs_pyscript_tmp/")
+        with open(self.temp_path, "w+") as f:
+            f.write(mini_coi_content)
 
     def on_page_content(self, html: str, *, page: Page, config: MkDocsConfig, files: Files) -> str | None:
         soup = BeautifulSoup(html, features="html.parser")
         code_blocks = soup.findAll(name=['code', 'div'], attrs={'class', 'language-py'})
         for block in code_blocks:
-            self.logger.info(f"Adding button to {page.canonical_url}")
+            #self.logger.info(f"Adding button to {page.canonical_url}")
             #Wrap codeblock in a new div
             div = soup.new_tag('div')
             div['class'] = "py-wrapper"
@@ -51,10 +59,37 @@ class Plugin(BasePlugin[MyPluginConfig]):
         soup = BeautifulSoup(output, features="html.parser")
         codeblocks = soup.find_all(attrs={"class": "py-wrapper" },)
         if (len(codeblocks)):
-            self.logger.info(f"Page at {page.canonical_url} will get script in head")
-            script = soup.new_tag("script")
-            script['type'] = "module"
-            script['src'] = SCRIPT.format(version=DEFAULT_VERSION)
-            soup.head.append(script)
+            #self.logger.info(f"Page at {page.canonical_url} will get script in head")
+            # Add PySript
+            py_script = soup.new_tag("script")
+            py_script['type'] = "module"
+            py_script['src'] = SCRIPT.format(version=DEFAULT_VERSION)
+            soup.head.append(py_script)
+
+            coi_script = soup.new_tag("script")
+            coi_script['src'] = '/mini-coi.js'
+            soup.head.append(coi_script)
         return str(soup)
+    
+    def on_files(self, files: Files, *, config: MkDocsConfig) -> Files | None:
+        #Add mini-coi to enable webworkers  
+
+        new_file = File(
+                path = self.temp_path,
+                src_dir = './',
+                dest_dir= config.site_dir,
+                use_directory_urls=False,
+                dest_uri="./mini-coi.js"
+            )
+        print(new_file)
+        files.append(new_file)
+
+        return files
+
+    def on_post_build(self, *, config: MkDocsConfig) -> None:
+        #self.temp_coi_file.close()
+        self.logger.info("foo")
+        os.remove("./_mkdocs_pyscript_tmp/mini_coi.js")
+        os.rmdir("./_mkdocs_pyscript_tmp/")
+        
             
