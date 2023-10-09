ab4f9c400a369f1915c2c644909d9505e8b03d5b
Jeff Glass <glass.jeffrey@gmail.com>
2023-10-09 14:34:07 -0500

    Still no theming

--- mkdocs_pyscript/js/makeblocks.js
+++ mkdocs_pyscript/js/makeblocks.js
@@ -1,11 +1,13 @@
-import { basicSetup, EditorView } from 'https://cdn.jsdelivr.net/npm/codemirror@6.0.1/+esm'
-import { EditorState, Compartment } from 'https://cdn.jsdelivr.net/npm/@codemirror/state@6.2.1/+esm'
+import { basicSetup, EditorView } from 'https://cdn.jsdelivr.net/npm/codemirror/+esm'
+import { EditorState, Compartment } from 'https://cdn.jsdelivr.net/npm/@codemirror/state/+esm'
 import { python } from 'https://cdn.jsdelivr.net/npm/@codemirror/lang-python/+esm';
 import { indentUnit } from 'https://cdn.jsdelivr.net/npm/@codemirror/language/+esm';
 import { keymap } from 'https://cdn.jsdelivr.net/npm/@codemirror/view@6.9.3/+esm';
 import { defaultKeymap } from 'https://cdn.jsdelivr.net/npm/@codemirror/commands@6.2.2/+esm';
 import {$, $$} from 'https://cdn.jsdelivr.net/npm/basic-devtools@0.1.6/+esm'
 import { PyWorker } from "https://cdn.jsdelivr.net/npm/@pyscript/core";
+import { basicLight } from 'https://cdn.jsdelivr.net/npm/@jimblackler/cm6-theme-basic-light@0.2.1/+esm'
+import { oneDarkTheme } from 'https://cdn.jsdelivr.net/npm/@codemirror/theme-one-dark/+esm';
 
 const RUNBUTTON = `<svg style="height:20px;width:20px;vertical-align:-.125em;transform-origin:center;overflow:visible;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>`;
 
@@ -75,6 +77,7 @@ class PyRepl extends HTMLElement {
                 { key: 'Ctrl-Enter', run: this.execute.bind(this), preventDefault: true },
                 { key: 'Shift-Enter', run: this.execute.bind(this), preventDefault: true },
             ]),
+            basicLight,
             basicSetup,
         ];
 
