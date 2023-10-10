c60d6196c13badcb6a6f22d3b15a9ffa9e5ecc16
Jeff Glass <glass.jeffrey@gmail.com>
2023-10-10 15:24:09 -0500

    Fix Unions, remove extra logging

--- mkdocs_pyscript/plugin.py
+++ mkdocs_pyscript/plugin.py
@@ -26,7 +26,7 @@ class Plugin(BasePlugin[MyPluginConfig]):
         self.total_time = 0
         
 
-    def on_config(self, config: MkDocsConfig) -> Union(MkDocsConfig, None):
+    def on_config(self, config: MkDocsConfig) -> Union[MkDocsConfig, None]:
         # Append static resources
         config["theme"].dirs.append(glr_path_static("dist/js"))
         config["theme"].dirs.append(glr_path_static("css"))
@@ -36,16 +36,15 @@ class Plugin(BasePlugin[MyPluginConfig]):
 
         # Set version
         self.SCRIPT_LINK = SCRIPT.format(version=self.config.pyscript_version)
-        self.logger.info("Script Link: " + self.SCRIPT_LINK)
 
         # Disable navigation.instant
         if config['theme']['features'] and 'navigation.instant' in config['theme']['features']:
-            self.logger.warning("mkdocs-pyscript is not ")
+            self.logger.warning("mkdocs-pyscript is not compatible with navigation.instant; instant navigation will be disabled.")
             config['theme']['features'].remove('navigation.instant')
         
         return config
 
-    def on_page_content(self, html: str, *, page: Page, config: MkDocsConfig, files: Files) -> Union(str, None):
+    def on_page_content(self, html: str, *, page: Page, config: MkDocsConfig, files: Files) -> Union[str, None]:
         soup = BeautifulSoup(html, features="html.parser")
         code_blocks = soup.findAll(name=['code', 'div'], attrs={'class', 'language-py'})
         for block in code_blocks:
@@ -58,7 +57,7 @@ class Plugin(BasePlugin[MyPluginConfig]):
             block.wrap(div) # Wrap codeblock with div
         return str(soup)
     
-    def on_post_page(self, output: str, *, page: Page, config: MkDocsConfig) -> Union(str, None):
+    def on_post_page(self, output: str, *, page: Page, config: MkDocsConfig) -> Union[str, None]:
         soup = BeautifulSoup(output, features="html.parser")
         codeblocks = soup.find_all(attrs={"class": "py-wrapper" },)
         if (len(codeblocks)):
