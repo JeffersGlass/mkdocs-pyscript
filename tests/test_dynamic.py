3e75788b3446d4899f96fbf9e449a0f10bb1812c
Jeff Glass <glass.jeffrey@gmail.com>
2023-10-27 09:53:48 -0500

    Better run test

--- tests/test_dynamic.py
+++ tests/test_dynamic.py
@@ -15,7 +15,7 @@ class TestDynamic(MkdocsPyscriptTest):
         expect(page).to_have_title("Example")
 
     #@pytest.mark.parametrize('dir', [('./basic')])
-    def test_injected_buttons(self, page: Page, dev_server):
+    def test_codemirror_and_run(self, page: Page, dev_server):
         filepath = self.build_site("basic")
         with open(self._index_file, "r") as f:
             page.goto(str(Path(dev_server.base_url) / filepath / "index.html"))
@@ -23,7 +23,20 @@ class TestDynamic(MkdocsPyscriptTest):
         wrappers = page.query_selector_all('.py-wrapper')
         assert len(wrappers) == 3
 
+        # Check that all buttons load
         button_selector = '[data-pyscript="button"]'
         page.wait_for_selector(button_selector)
         buttons = page.query_selector_all(button_selector)
         assert len(buttons) == 3
+
+        # Check that button loads the code in a codemirror
+        buttons[0].click()
+        codemirror = page.locator("div .cm-editor")
+        assert """# This is a 'py' blockprint("hello_py")""" in codemirror.text_content()
+
+        # Check that py-repl actually runs and emits output
+        page.locator(".py-repl-run-button").click()
+        page.wait_for_selector(".py-repl-output")
+        assert page.locator(".py-repl-output").text_content() == "hello_py"
+
+
